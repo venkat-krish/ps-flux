8c67bc7da597f0081d851110a320e26b78b9a7dd
Venkat-krish <venkat.krishnan@live.com>
2020-04-12 12:30:00 +0530

    Properties and basics of API call

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -2,11 +2,21 @@ import React from "react";
 import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
+  state = {
+    courses: [],
+  };
+
   constructor(props) {
     super(props); // Calling the parent constructor
     this.state = {
       courses: [],
     };
+  }
+
+  // Component life-cycle method
+  // Proper life cycle method to call API
+  componentDidMount() {
+    getCourses().then((courses) => this.setState({ courses: courses }));
   }
 
   render() {
